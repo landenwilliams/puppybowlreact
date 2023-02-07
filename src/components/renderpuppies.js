dfb5f9926ce8aaed6d9e98a9b6640c930e06c3a2
landen <landenjwilliams@gmail.com>
2023-02-07 00:55:35 -0600

    rerender functional

--- src/components/renderpuppies.js
+++ src/components/renderpuppies.js
@@ -6,6 +6,7 @@ const RenderPuppies =  () => {
   let puppiesLoaded = false;
   const [puppies, setPuppies] = useState([]);
   const [visible, setVisible] = useState(true);
+
   
   useEffect(() => {
     const fetchPuppies = async () => {
@@ -34,10 +35,9 @@ const RenderPuppies =  () => {
   const detailFunction = (pup) => {
 
     setVisible((prev) => !prev);
-    // console.log(pup);
 
     root.render( 
-              <><div className="single-player-view" >
+              <>{visible && ( <div className="single-player-view" >
               <div className="header-info">
                 <p className="pup-title">{pup.name}</p>
                 <p className="pup-number">{pup.id}</p>
@@ -45,10 +45,37 @@ const RenderPuppies =  () => {
               <p>Team: {pup.team ? pup.team.name : 'Unassigned'}</p>
                 <p>Breed: {pup.breed}</p>
                 <img src={pup.imageUrl} />
-              <button id="see-all">Back to all players</button>
-            </div></>)
+              <button id="see-all" onClick={goBackFunction}>Back to all players</button>
+            </div>)}</>)
     
   }
+
+  const goBackFunction = () => {
+
+    root.render (<div id='playerContainer'>{
+
+      puppies.map((pup,index) => {
+        
+        return  <React.Fragment key={index} >
+                  
+                    <div key={index} id={pup.id} className="single-player-card">
+                      <div className="header-info">
+                        <p className="pup-title">{pup.name}</p>
+                        <p className="pup-number">{pup.id}</p>
+                      </div>
+                      <img src={pup.imageUrl} />
+                      <button className="detail-button" id={pup.id} value="" onClick={() => detailFunction(pup)}>See details</button> 
+                      <button className="delete-button" data-id={pup.id} onClick={deleteFunction}>Delete</button>
+                    </div>
+                  
+                </React.Fragment>
+              
+      }
+      
+    )}
+    </div>)
+  }
+
   if (!puppies || !puppies.length) {
     return (
     <h1>Loading puppies..</h1>
